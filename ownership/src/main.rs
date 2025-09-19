95aa9bca9241ef7026ee42319c69acd3d383bd2d
Erick Gabriel <erickgabrielalves@hotmail.com>
2025-09-19 11:55:02 -0300

    feat: ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,23 @@
+// fn main() {
+//     let s1 = String::from("hello");
+
+//     let len = calculate_length(&s1);
+
+//     println!("The length of '{s1}' is {len}.");
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
+fn main() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+
+    println!("{some_string}");
+}
---
+++ slice-type/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let s = String::from("Erick Gabriel");
+
+    let word = first_word(&s);
+
+    println!("{word}");
+
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("{hello}");
+    println!("{world}");
+
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
